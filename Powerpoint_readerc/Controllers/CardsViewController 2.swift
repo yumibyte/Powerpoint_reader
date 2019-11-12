e4c3c8af10ca90357dbd22537115813ad383238a
Yumibyte <ashraigosa@gmail.com>
2019-11-11 20:52:19 -0800

    navigation controller and segue connection between view controllers

---
+++ Powerpoint_readerc/Controllers/CardsViewController 2.swift
@@ -0,0 +1,35 @@
+//
+//  CardsViewController.swift
+//  
+//
+//  Created by Ashley Raigosa on 11/10/19.
+//
+
+import UIKit
+import Firebase
+class CardsViewController: UIViewController {
+
+    // Instance variables
+    var flashCardArray: [Card] = [Card]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Shadow of card
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
